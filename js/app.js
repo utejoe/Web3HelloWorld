39cc53e0b4e0f9dbe1fa55f085f0233214722c05
Uyi Obazee <utejoe.ju@gmail.com>
2023-11-21 18:02:22 +0100

    committing changes to website

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545'); // Connect to your Ethereum node
+    console.log('DOMContentLoaded event triggered');
 
-    // Replace with your actual contract address and ABI
-    const contractAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+    console.log('Web3 instance created:', web3);
+
+    const contractAddress = '0x4815A8Ba613a3eB21A920739dE4cA7C439c7e1b1';
+    console.log('Contract Address:', contractAddress);
+
     const contractABI = [
+        {
+            "inputs": [],
+            "name": "getGreeting",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
         {
             "inputs": [],
             "name": "greeting",
@@ -27,20 +44,17 @@ document.addEventListener('DOMContentLoaded', async () => {
             const defaultAccount = accounts[0];
 
             // Execute your smart contract function here
-            const result = await myContract.methods.greeting().call({ from: defaultAccount });
+            const result = await myContract.methods.getGreeting().call({ from: defaultAccount });
 
             console.log('Smart Contract Executed Successfully:', result);
 
-            // Handle the success communication here, e.g., display a message
-            alert('Smart Contract Executed Successfully: ' + result);
+            // Display the result in the alert box
+            document.getElementById('resultAlert').innerHTML = `<div class="alert-success">${result}</div>`;
         } catch (error) {
             console.error('Error executing smart contract:', error);
 
-            // Log the error details to the console
-            console.log('Error Details:', error.details);
-
-            // Handle the error communication here, e.g., display an error message
-            alert('Error executing smart contract. Check the console for details.');
+            // Display the error in a pop-up on the webpage
+            alert(`Error executing smart contract: ${error.message}`);
         }
     });
 });
